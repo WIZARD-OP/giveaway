daf855472bc2e71c98231d36cb6bf824b3f406b7
WIZARD-OP <79045012+WIZARD-OP@users.noreply.github.com>
2021-02-17 09:19:56 +0530

    Update end.js

--- commands/end.js
+++ commands/end.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
 
     // If the member doesn't have enough permissions
     if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
-        return message.channel.send(':x: You need to have the manage messages permissions to reroll giveaways.');
+        return message.channel.send('<:DS_BOT:811436200194998342> **You need to have the manage messages permissions to reroll giveaways.');
     }
 
     // If no message ID or giveaway name is specified
